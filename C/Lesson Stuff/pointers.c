7defec58eb835967f6a8da30be2d00c2f3e9af12
dwayne <dwayne.reid@us.af.mil>
2024-11-05 15:11:59 -0600

    corrected code to show proper values

--- C/Lesson Stuff/pointers.c
+++ C/Lesson Stuff/pointers.c
@@ -3,12 +3,12 @@
 int main() {
     int var = 10; //declare an int variable
     int *ptr; //declare a pointer to an int.
-    ptr = &var; // initialize pointer to the address of var
+    ptr = &var; // initialize pointer to the address of var (ampersand allows that)
 
     //displays the values
     printf("Value of var: %d\n", var);
     printf("Address of var: %p\n", (void*)&var);
-    printf("Value stored in ptr: %d\n", (void*)ptr);
+    printf("Value stored in ptr: %p\n", (void*)ptr);
     printf("Value pointed to by ptr: %d\n", *ptr); //dereference ptr to get vars value
 
     return 0;
